8c6628fbfc68f2731fc20572109c5394b729dc35
OhhhZenix <ahom725@gmail.com>
2026-02-01 17:27:10 -0700

    Update WynnbindsModMenu.java

--- src/client/java/dev/zenix/wynnbinds/client/WynnbindsModMenu.java
+++ src/client/java/dev/zenix/wynnbinds/client/WynnbindsModMenu.java
@@ -1,6 +1,18 @@
 package dev.zenix.wynnbinds.client;
 
-public class WynnbindsModMenu {
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.minecraft.client.gui.screen.Screen;
 
-    
+public class WynnbindsModMenu implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return WynnbindsModMenu::createScreen;
+    }
+
+    private static Screen createScreen(Screen screen) {
+
+        return screen;
+    }
 }
