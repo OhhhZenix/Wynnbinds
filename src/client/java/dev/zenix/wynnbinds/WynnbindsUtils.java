2a4b7b2f5b9de85119b6a46fcb36ce5e197db7e9
OhhhZenix <ahom725@gmail.com>
2026-02-04 08:40:52 -0700

    Update WynnbindsUtils.java

--- src/client/java/dev/zenix/wynnbinds/WynnbindsUtils.java
+++ src/client/java/dev/zenix/wynnbinds/WynnbindsUtils.java
@@ -1,18 +1,33 @@
 package dev.zenix.wynnbinds;
 
+import java.util.Locale;
+import java.util.regex.Pattern;
+
 public class WynnbindsUtils {
 
+    public static String capitalizeStartOfEachWord(String input) {
+        if (input == null || input.isEmpty())
+            return input;
+
+        String output = Pattern
+                .compile("\\b\\p{L}")
+                .matcher(input.toLowerCase())
+                .replaceAll(match -> match.group().toUpperCase(Locale.ROOT));
+
+        return output;
+    }
+
     public static String getKeyName(String key) {
-        if (key == null || key.isEmpty())
-            return "Unknown";
+        if (key.equals("key.keyboard.unknown"))
+            return "Unbound";
 
         if (key.startsWith("key.keyboard."))
-            return key.substring("key.keyboard.".length()).toUpperCase();
+            return capitalizeStartOfEachWord(key.substring("key.keyboard.".length()));
 
         if (key.startsWith("key.mouse."))
-            return String.format("MOUSE BUTTON %s", key.substring("key.mouse.".length()).toUpperCase());
+            return capitalizeStartOfEachWord(String.format("MOUSE BUTTON %s", key.substring("key.mouse.".length())));
 
-        return "Unbound";
+        return "Unknown";
     }
 
 }
