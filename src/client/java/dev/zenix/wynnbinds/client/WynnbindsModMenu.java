99bc5648507b285cf5884bf1218b008aecca0627
OhhhZenix <ahom725@gmail.com>
2026-02-04 19:01:09 -0700

    Update WynnbindsModMenu.java

--- src/client/java/dev/zenix/wynnbinds/client/WynnbindsModMenu.java
+++ src/client/java/dev/zenix/wynnbinds/client/WynnbindsModMenu.java
@@ -10,59 +10,65 @@ import net.minecraft.text.Text;
 
 public class WynnbindsModMenu implements ModMenuApi {
 
-    @Override
-    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> {
-            ConfigBuilder builder = ConfigBuilder.create().setParentScreen(parent).setTitle(Text.of("Wynnbinds"));
-            builder.setSavingRunnable(WynnbindsClient.getInstance()::saveConfig);
+        @Override
+        public ConfigScreenFactory<?> getModConfigScreenFactory() {
+                return parent -> {
+                        ConfigBuilder builder = ConfigBuilder.create().setParentScreen(parent)
+                                        .setTitle(Text.of("Wynnbinds"));
+                        builder.setSavingRunnable(WynnbindsClient.getInstance()::saveConfig);
 
-            ConfigEntryBuilder entryBuilder = builder.entryBuilder();
-            ConfigCategory configCategory = builder.getOrCreateCategory(Text.of("Config"));
-            var config = WynnbindsClient.getInstance().getConfig();
+                        var config = WynnbindsClient.getInstance().getConfig();
+                        ConfigEntryBuilder entryBuilder = builder.entryBuilder();
 
-            // General
-            configCategory.addEntry(entryBuilder.startTextDescription(Text.of("General")).build());
-            configCategory.addEntry(entryBuilder
-                    .startBooleanToggle(Text.of("Enable Mod"), config.isModEnabled())
-                    .setTooltip(Text.of("Enable or disable the Wynnbinds mod"))
-                    .setDefaultValue(true)
-                    .setSaveConsumer(value -> config.setEnableMod(value)).build());
-            configCategory.addEntry(entryBuilder
-                    .startBooleanToggle(Text.of("Enable Bind Notifications"), config.isBindNotificationsEnabled())
-                    .setTooltip(Text.of("Enable or disable bind notifications"))
-                    .setDefaultValue(true)
-                    .setSaveConsumer(value -> config.setEnableBindNotifications(value))
-                    .build());
-            configCategory.addEntry(entryBuilder
-                    .startBooleanToggle(Text.of("Enable Update Notifications"), config.isUpdateNotificationsEnabled())
-                    .setTooltip(Text.of("Enable or disable update notifications"))
-                    .setDefaultValue(true)
-                    .setSaveConsumer(value -> config.setEnableUpdateNotifications(value))
-                    .build());
+                        // General
+                        ConfigCategory generalCategory = builder.getOrCreateCategory(Text.of("General"));
+                        generalCategory.addEntry(entryBuilder
+                                        .startBooleanToggle(Text.of("Enable Mod"), config.isModEnabled())
+                                        .setTooltip(Text.of("Enable or disable the Wynnbinds mod"))
+                                        .setDefaultValue(true)
+                                        .setSaveConsumer(value -> config.setEnableMod(value)).build());
+                        generalCategory.addEntry(entryBuilder
+                                        .startBooleanToggle(Text.of("Enable Bind Notifications"),
+                                                        config.isBindNotificationsEnabled())
+                                        .setTooltip(Text.of("Enable or disable bind notifications"))
+                                        .setDefaultValue(true)
+                                        .setSaveConsumer(value -> config.setEnableBindNotifications(value))
+                                        .build());
+                        generalCategory.addEntry(entryBuilder
+                                        .startBooleanToggle(Text.of("Enable Update Notifications"),
+                                                        config.isUpdateNotificationsEnabled())
+                                        .setTooltip(Text.of("Enable or disable update notifications"))
+                                        .setDefaultValue(true)
+                                        .setSaveConsumer(value -> config.setEnableUpdateNotifications(value))
+                                        .build());
 
-            // Keybinds
-            configCategory.addEntry(entryBuilder.startTextDescription(Text.of("Default Keybinds")).build());
-            for (Wynnbinds bind : Wynnbinds.values()) {
-                String translationKey = bind.getTranslationKey();
-                InputUtil.Key currentKey = InputUtil
-                        .fromTranslationKey(config.getDefaultKeyBind(bind.getTranslationKey()));
-                InputUtil.Key defaultKey = InputUtil.fromTranslationKey(bind.getDefaultBoundKey());
-                configCategory.addEntry(entryBuilder
-                        .startKeyCodeField(Text.of(bind.getDisplayName()), currentKey)
-                        .setTooltip(Text.of(String.format("Set default keybind for %s", bind.getDisplayName())))
-                        .setDefaultValue(defaultKey)
-                        .setKeySaveConsumer(value -> {
-                            String boundKey = value.getTranslationKey();
-                            WynnbindsClient.LOGGER.debug(
-                                    "Setting keybind for {} to {}",
-                                    translationKey, boundKey);
-                            config.setDefaultKeyBind(translationKey, boundKey);
-                        })
-                        .build());
-            }
+                        builder.getOrCreateCategory(Text.of("Default Keybinds"));
 
-            return builder.build();
-        };
-    }
+                        // Keybinds
+                        ConfigCategory defaultKeyBindsCategory = builder
+                                        .getOrCreateCategory(Text.of("Default Keybinds"));
+                        for (Wynnbinds bind : Wynnbinds.values()) {
+                                String translationKey = bind.getTranslationKey();
+                                InputUtil.Key currentKey = InputUtil
+                                                .fromTranslationKey(config.getDefaultKeyBind(bind.getTranslationKey()));
+                                InputUtil.Key defaultKey = InputUtil.fromTranslationKey(bind.getDefaultBoundKey());
+                                defaultKeyBindsCategory.addEntry(entryBuilder
+                                                .startKeyCodeField(Text.of(bind.getDisplayName()), currentKey)
+                                                .setTooltip(Text.of(String.format("Set default keybind for %s",
+                                                                bind.getDisplayName())))
+                                                .setDefaultValue(defaultKey)
+                                                .setKeySaveConsumer(value -> {
+                                                        String boundKey = value.getTranslationKey();
+                                                        WynnbindsClient.LOGGER.debug(
+                                                                        "Setting keybind for {} to {}",
+                                                                        translationKey, boundKey);
+                                                        config.setDefaultKeyBind(translationKey, boundKey);
+                                                })
+                                                .build());
+                        }
+
+                        return builder.build();
+                };
+        }
 
 }
