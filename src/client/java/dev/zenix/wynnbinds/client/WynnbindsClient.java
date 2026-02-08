a44e6a4f496ae50f6031aed2fa068299f4192222
OhhhZenix <ahom725@gmail.com>
2026-02-08 16:51:21 -0700

    wip

--- src/client/java/dev/zenix/wynnbinds/client/WynnbindsClient.java
+++ src/client/java/dev/zenix/wynnbinds/client/WynnbindsClient.java
@@ -6,6 +6,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
@@ -15,9 +16,10 @@ import org.apache.logging.log4j.core.config.Configurator;
 public class WynnbindsClient implements ClientModInitializer {
 
     public static final String MOD_ID = "wynnbinds";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
     private static WynnbindsClient instance = null;
-    private Logger logger;
-    private WynnbindsConfig config;
+    private WynnbindsConfig config = null;
+    private String oldCharacterId = WynnbindsUtils.DUMMY_CHARACTER_ID;
 
     public static WynnbindsClient getInstance() {
         return instance;
@@ -26,10 +28,14 @@ public class WynnbindsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         instance = this;
-        loadLogger();
+        setupLogger();
         loadConfig();
         startUpdateChecker();
         ClientTickEvents.END_CLIENT_TICK.register(client -> onEndClientTick(client));
+    }
+
+    private void setupLogger() {
+        Configurator.setLevel(LOGGER.getName(), Level.DEBUG);
     }
 
     private void startUpdateChecker() {
@@ -38,77 +44,94 @@ public class WynnbindsClient implements ClientModInitializer {
         thread.start();
     }
 
-    public Logger getLogger() {
-        return logger;
-    }
-
-    private void loadLogger() {
-        Configurator.setLevel(logger.getName(), Level.DEBUG);
-        logger = LogManager.getLogger(MOD_ID);
-    }
-
     public WynnbindsConfig getConfig() {
         return config;
     }
 
     public void saveConfig() {
-        logger.debug("Saving configuration");
+        LOGGER.debug("Saving configuration");
         AutoConfig.getConfigHolder(WynnbindsConfig.class).save();
     }
 
     private void loadConfig() {
         AutoConfig.register(WynnbindsConfig.class, GsonConfigSerializer::new);
         config = AutoConfig.getConfigHolder(WynnbindsConfig.class).getConfig();
-        logger.info("Config loaded successfully");
+        LOGGER.info("Config loaded successfully");
     }
 
     private void onEndClientTick(MinecraftClient client) {
-        String currentCharacter = WynnbindsUtils.getCharacterId();
-        var shouldSaveConfig = false;
+        var newCharacterId = WynnbindsUtils.getCharacterId();
 
         // Is it a valid character?
-        if (currentCharacter.equals(WynnbindsUtils.DUMMY_CHARACTER_ID)) {
+        if (newCharacterId.equals(WynnbindsUtils.DUMMY_CHARACTER_ID)) {
             return;
         }
 
         // Is it a new character?
-        if (!config.hasCharacter(currentCharacter)) {
-            // from wynnbinds to minecraft
-            config.setKeys(currentCharacter, config.getDefaultKeys());
-            shouldSaveConfig = true;
+        if (!oldCharacterId.equals(newCharacterId)) {
+            LOGGER.debug("Character changed from '{}' to '{}'", oldCharacterId, newCharacterId);
+
+            // Is it an existing character?
+            if (!config.hasCharacter(newCharacterId)) {
+                LOGGER.debug("Not an existing character. Using default keybinds.");
+                config.setKeys(newCharacterId, config.getDefaultKeys());
+                saveConfig();
+            }
+
+            // load keybinds
+            for (KeyBinding keyBinding : WynnbindsUtils.getKeybindingsFromCaptureKeys()) {
+                var translationKey = keyBinding.getTranslationKey();
+                var boundKey = config.getDefaultKey(translationKey);
+                var key = InputUtil.fromTranslationKey(boundKey);
+                keyBinding.setBoundKey(key);
+                LOGGER.debug("Loaded keybind for {}", translationKey);
+            }
+
+            // update
+            KeyBinding.updateKeysByCode();
+            LOGGER.debug("Refreshed keybinds.");
+
+            // save
+            client.options.write();
+            LOGGER.debug("Saved keybinds.");
+
+            // update tracking
+            oldCharacterId = newCharacterId;
         }
 
         // from minecraft to wynnbinds
-        var keys = config.getKeys(currentCharacter);
-        for (KeyBinding keyBinding : WynnbindsUtils.getKeybindingsFromCaptureKeys()) {
-            String translationKey = keyBinding.getTranslationKey();
+        // var keys = config.getKeys(newCharacterId);
+        // for (KeyBinding keyBinding : WynnbindsUtils.getKeybindingsFromCaptureKeys())
+        // {
+        // String translationKey = keyBinding.getTranslationKey();
 
-            // Is it a new keybind?
-            if (keys.containsKey(translationKey)) {
-                var defaultKey = config.getDefaultKey(translationKey);
-                keys.put(translationKey, defaultKey);
-                shouldSaveConfig = true;
-                logger.debug("Detected new keybind: {} = {}", translationKey, defaultKey);
-                continue;
-            }
+        // // Is it a new keybind?
+        // if (keys.containsKey(translationKey)) {
+        // var defaultKey = config.getDefaultKey(translationKey);
+        // keys.put(translationKey, defaultKey);
+        // shouldSaveConfig = true;
+        // logger.debug("Detected new keybind: {} = {}", translationKey, defaultKey);
+        // continue;
+        // }
 
-            String newBoundKey = keyBinding.getBoundKeyTranslationKey();
-            String oldBoundKey = keys.get(translationKey);
+        // String newBoundKey = keyBinding.getBoundKeyTranslationKey();
+        // String oldBoundKey = keys.get(translationKey);
 
-            // Has the mapping changed?
-            if (newBoundKey.equals(oldBoundKey)) {
-                continue;
-            }
+        // // Has the mapping changed?
+        // if (newBoundKey.equals(oldBoundKey)) {
+        // continue;
+        // }
 
-            keys.put(translationKey, newBoundKey);
-            shouldSaveConfig = true;
+        // keys.put(translationKey, newBoundKey);
+        // shouldSaveConfig = true;
 
-            logger.debug("Updated keybind for '{}' from '{}' to '{}'", translationKey, oldBoundKey, newBoundKey);
-        }
+        // logger.debug("Updated keybind for '{}' from '{}' to '{}'", translationKey,
+        // oldBoundKey, newBoundKey);
+        // }
 
-        if (shouldSaveConfig) {
-            saveConfig();
-        }
+        // if (shouldSaveConfig) {
+        // saveConfig();
+        // }
     }
 
 }
