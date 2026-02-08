8ced4b74ee56430c3ac6cf37d0b74598badc3736
OhhhZenix <ahom725@gmail.com>
2026-02-08 08:53:36 -0700

    Update WynnbindsUtils.java

--- src/client/java/dev/zenix/wynnbinds/client/WynnbindsUtils.java
+++ src/client/java/dev/zenix/wynnbinds/client/WynnbindsUtils.java
@@ -2,7 +2,6 @@ package dev.zenix.wynnbinds.client;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.regex.Pattern;
