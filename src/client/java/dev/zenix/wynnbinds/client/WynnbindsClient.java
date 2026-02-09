173b31c629d4cc847e0c9cd5714ea9b303a5f041
OhhhZenix <ahom725@gmail.com>
2026-02-08 19:22:35 -0700

    Update WynnbindsClient.java

--- src/client/java/dev/zenix/wynnbinds/client/WynnbindsClient.java
+++ src/client/java/dev/zenix/wynnbinds/client/WynnbindsClient.java
@@ -5,7 +5,6 @@ import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 
 import org.apache.logging.log4j.Level;
@@ -88,12 +87,10 @@ public class WynnbindsClient implements ClientModInitializer {
             }
 
             // update
-            KeyBinding.updateKeysByCode();
-            LOGGER.debug("Refreshed keybinds.");
+            WynnbindsUtils.refreshKeyBindings();
 
             // save
-            client.options.write();
-            LOGGER.debug("Saved keybinds.");
+            WynnbindsUtils.saveKeyBindings();
         }
 
         LOGGER.debug("Scanning for keybind changes.");
