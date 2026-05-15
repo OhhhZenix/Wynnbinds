5ac38fc336cc497ca912f1d9e9eca2afe4fa7a87
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2026-05-15 11:37:00 -0600

    Update WynnbindsConfigScreen.java

--- src/client/java/dev/zenix/wynnbinds/client/WynnbindsConfigScreen.java
+++ src/client/java/dev/zenix/wynnbinds/client/WynnbindsConfigScreen.java
@@ -1,5 +1,6 @@
 package dev.zenix.wynnbinds.client;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
@@ -7,57 +8,55 @@ import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import me.shedaniel.clothconfig2.impl.builders.SubCategoryBuilder;
-
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 
 public class WynnbindsConfigScreen {
 
         public static Screen create(Screen parent) {
-                ConfigBuilder builder = ConfigBuilder.create().setParentScreen(parent).setTitle(Text.of("Wynnbinds"));
+                ConfigBuilder builder = ConfigBuilder.create().setParentScreen(parent).setTitle(Component.nullToEmpty("Wynnbinds"));
                 builder.setSavingRunnable(WynnbindsClient.getInstance()::saveConfig);
 
                 WynnbindsConfig config = WynnbindsClient.getInstance().getConfig();
                 ConfigEntryBuilder entryBuilder = builder.entryBuilder();
 
                 // General
-                ConfigCategory generalCategory = builder.getOrCreateCategory(Text.of("General"));
+                ConfigCategory generalCategory = builder.getOrCreateCategory(Component.nullToEmpty("General"));
                 generalCategory.addEntry(
-                                entryBuilder.startBooleanToggle(Text.of("Wynnbinds"), config.isModEnabled())
-                                                .setTooltip(Text.of("Enable or disable the mod")).setDefaultValue(true)
+                                entryBuilder.startBooleanToggle(Component.nullToEmpty("Wynnbinds"), config.isModEnabled())
+                                                .setTooltip(Component.nullToEmpty("Enable or disable the mod")).setDefaultValue(true)
                                                 .setSaveConsumer(value -> config.setEnableMod(value)).build());
                 generalCategory.addEntry(entryBuilder
-                                .startBooleanToggle(Text.of("Bind Notifications"),
+                                .startBooleanToggle(Component.nullToEmpty("Bind Notifications"),
                                                 config.isBindNotificationsEnabled())
-                                .setTooltip(Text.of("Enable or disable bind notifications")).setDefaultValue(true)
+                                .setTooltip(Component.nullToEmpty("Enable or disable bind notifications")).setDefaultValue(true)
                                 .setSaveConsumer(value -> config.setEnableBindNotifications(value)).build());
                 generalCategory.addEntry(entryBuilder
-                                .startBooleanToggle(Text.of("Update Notifications"),
+                                .startBooleanToggle(Component.nullToEmpty("Update Notifications"),
                                                 config.isUpdateNotificationsEnabled())
-                                .setTooltip(Text.of("Enable or disable update notifications")).setDefaultValue(true)
+                                .setTooltip(Component.nullToEmpty("Enable or disable update notifications")).setDefaultValue(true)
                                 .setSaveConsumer(value -> config.setEnableUpdateNotifications(value)).build());
 
                 // Capture
-                ConfigCategory captureKeysCategory = builder.getOrCreateCategory(Text.of("Capture"));
+                ConfigCategory captureKeysCategory = builder.getOrCreateCategory(Component.nullToEmpty("Capture"));
                 HashMap<String, ArrayList<String>> allKeysByCategory = WynnbindsUtils.getAllKeysByCategory();
 
                 for (Entry<String, ArrayList<String>> entry : allKeysByCategory.entrySet()) {
                         String category = entry.getKey();
                         ArrayList<String> translationKeys = entry.getValue();
 
-                        Text categoryText = Text.translatable(category);
+                        Component categoryText = Component.translatable(category);
                         SubCategoryBuilder subCategory = entryBuilder.startSubCategory(categoryText);
 
-                        subCategory.setTooltip(Text.of("Keys relating to " + categoryText.getString()));
+                        subCategory.setTooltip(Component.nullToEmpty("Keys relating to " + categoryText.getString()));
 
                         for (String translationKey : translationKeys) {
-                                Text keyText = Text.translatable(translationKey);
+                                Component keyText = Component.translatable(translationKey);
 
                                 subCategory.add(entryBuilder
                                                 .startBooleanToggle(keyText, config.isCaptureKey(translationKey))
-                                                .setTooltip(Text.of(
+                                                .setTooltip(Component.nullToEmpty(
                                                                 "Enable or disable capture for " + keyText.getString()))
                                                 .setDefaultValue(false).setSaveConsumer(value -> {
                                                         if (value) {
@@ -72,28 +71,28 @@ public class WynnbindsConfigScreen {
                 }
 
                 // Default
-                ConfigCategory defaultKeysCategory = builder.getOrCreateCategory(Text.of("Default"));
+                ConfigCategory defaultKeysCategory = builder.getOrCreateCategory(Component.nullToEmpty("Default"));
                 for (Entry<String, ArrayList<String>> entry : WynnbindsUtils.getCaptureKeysByCategory()
                                 .entrySet()) {
                         String category = entry.getKey();
                         ArrayList<String> translationKeys = entry.getValue();
 
-                        Text categoryText = Text.translatable(category);
+                        Component categoryText = Component.translatable(category);
                         SubCategoryBuilder subCategory = entryBuilder.startSubCategory(categoryText);
 
-                        subCategory.setTooltip(Text.of("Keys relating to " + categoryText.getString()));
+                        subCategory.setTooltip(Component.nullToEmpty("Keys relating to " + categoryText.getString()));
 
                         for (String translationKey : translationKeys) {
-                                Text keyText = Text.translatable(translationKey);
-                                InputUtil.Key currentKey = InputUtil
-                                                .fromTranslationKey(config.getDefaultKey(translationKey));
+                                Component keyText = Component.translatable(translationKey);
+                                InputConstants.Key currentKey = InputConstants
+                                                .getKey(config.getDefaultKey(translationKey));
                                 config.getDefaultKey(translationKey);
                                 subCategory.add(entryBuilder.startKeyCodeField(keyText, currentKey)
-                                                .setTooltip(Text.of(
+                                                .setTooltip(Component.nullToEmpty(
                                                                 String.format("Set default keybind for %s",
                                                                                 keyText.getString())))
-                                                .setDefaultValue(InputUtil.UNKNOWN_KEY).setKeySaveConsumer(value -> {
-                                                        String boundKey = value.getTranslationKey();
+                                                .setDefaultValue(InputConstants.UNKNOWN).setKeySaveConsumer(value -> {
+                                                        String boundKey = value.getName();
                                                         WynnbindsClient.LOGGER.debug("Setting keybind for {} to {}",
                                                                         translationKey, boundKey);
                                                         config.setDefaultKey(translationKey, boundKey);
@@ -106,37 +105,37 @@ public class WynnbindsConfigScreen {
                 // Current
                 String currentCharacterId = WynnbindsUtils.getCharacterId();
                 if (!currentCharacterId.equals(WynnbindsUtils.DUMMY_CHARACTER_ID)) {
-                        ConfigCategory currentKeysCategory = builder.getOrCreateCategory(Text.of("Current"));
+                        ConfigCategory currentKeysCategory = builder.getOrCreateCategory(Component.nullToEmpty("Current"));
                         for (Entry<String, ArrayList<String>> entry : WynnbindsUtils.getCaptureKeysByCategory()
                                         .entrySet()) {
                                 String category = entry.getKey();
                                 ArrayList<String> translationKeys = entry.getValue();
 
-                                Text categoryText = Text.translatable(category);
+                                Component categoryText = Component.translatable(category);
                                 SubCategoryBuilder subCategory = entryBuilder.startSubCategory(categoryText);
 
-                                subCategory.setTooltip(Text.of("Keys relating to " + categoryText.getString()));
+                                subCategory.setTooltip(Component.nullToEmpty("Keys relating to " + categoryText.getString()));
 
                                 for (String translationKey : translationKeys) {
-                                        InputUtil.Key currentKey = InputUtil
-                                                        .fromTranslationKey(config.getKey(currentCharacterId,
+                                        InputConstants.Key currentKey = InputConstants
+                                                        .getKey(config.getKey(currentCharacterId,
                                                                         translationKey));
-                                        InputUtil.Key defaultKey = InputUtil
-                                                        .fromTranslationKey(config.getDefaultKey(translationKey));
-                                        Text keyText = Text.translatable(translationKey);
+                                        InputConstants.Key defaultKey = InputConstants
+                                                        .getKey(config.getDefaultKey(translationKey));
+                                        Component keyText = Component.translatable(translationKey);
                                         subCategory.add(entryBuilder.startKeyCodeField(keyText, currentKey)
-                                                        .setTooltip(Text
-                                                                        .of(String.format("Set keybind for %s",
+                                                        .setTooltip(Component
+                                                                        .nullToEmpty(String.format("Set keybind for %s",
                                                                                         keyText.getString())))
                                                         .setDefaultValue(defaultKey).setKeySaveConsumer(value -> {
                                                                 // update our bind
-                                                                String boundKey = value.getTranslationKey();
+                                                                String boundKey = value.getName();
                                                                 config.setKey(currentCharacterId, translationKey,
                                                                                 boundKey);
 
                                                                 // update minecraft bind
-                                                                KeyBinding keyBinding = KeyBinding.byId(translationKey);
-                                                                keyBinding.setBoundKey(value);
+                                                                KeyMapping keyBinding = KeyMapping.get(translationKey);
+                                                                keyBinding.setKey(value);
                                                                 WynnbindsUtils.refreshAndSaveKeyBindings();
 
                                                                 // log
@@ -148,9 +147,9 @@ public class WynnbindsConfigScreen {
                                                                 // notify
                                                                 WynnbindsUtils
                                                                                 .sendNotification(
-                                                                                                Text.of(String.format(
+                                                                                                Component.nullToEmpty(String.format(
                                                                                                                 "Updated keybind for %s",
-                                                                                                                Text.translatable(
+                                                                                                                Component.translatable(
                                                                                                                                 translationKey)
                                                                                                                                 .getString())),
                                                                                                 config.isBindNotificationsEnabled());
