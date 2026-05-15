0f28f8615eeefdbcec6d53dd20c6bded88152adf
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2026-05-15 10:44:10 -0600

    Update Wynnbinds.java

--- src/main/java/dev/zenix/wynnbinds/Wynnbinds.java
+++ src/main/java/dev/zenix/wynnbinds/Wynnbinds.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 
 public class Wynnbinds implements ModInitializer {
 
+	public static final String MOD_NAME = "Wynnbinds";
 	public static final String MOD_ID = "wynnbinds";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
