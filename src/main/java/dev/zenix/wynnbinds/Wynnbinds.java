d367b4d897ef61305036cdabf049782c5a7ef298
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2026-05-15 10:42:05 -0600

    Update Wynnbinds.java

--- src/main/java/dev/zenix/wynnbinds/Wynnbinds.java
+++ src/main/java/dev/zenix/wynnbinds/Wynnbinds.java
@@ -2,8 +2,16 @@ package dev.zenix.wynnbinds;
 
 import net.fabricmc.api.ModInitializer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Wynnbinds implements ModInitializer {
 
-    @Override
-    public void onInitialize() {}
+	public static final String MOD_ID = "wynnbinds";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
 }
